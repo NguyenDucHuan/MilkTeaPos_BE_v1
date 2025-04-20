a0448daac0048f7376a2859bbdab8928b5ac5f7a
it-is-feasible <hiepmap1579@gmail.com>
2025-04-20 22:37:35 +0700

    order statistics

---
+++ MilkTeaPosManagement.Api/Controllers/StatisticController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilkTeaPosManagement.Api.Services.Interfaces;
+
+namespace MilkTeaPosManagement.Api.Controllers
+{
+    [Route("api/statistic")]
+    [ApiController]
+    public class StatisticController(IStatisticService service) : ControllerBase
+    {
+        private readonly IStatisticService _service = service;
+        [HttpGet("by-date")]
+        public async Task<IActionResult> GetByDate([FromBody] DateTime? date)
+        {
+            var result = await _service.OrderStatisticsByDate(date);
+            return Ok(result);
+        }
+        [HttpGet("by-week")]
+        public async Task<IActionResult> GetByWeek([FromBody] DateTime? fromDate, DateTime? toDate)
+        {
+            var result = await _service.OrderStatisticsByWeek(fromDate, toDate);
+            return Ok(result);
+        }
+        [HttpGet("by-month")]
+        public async Task<IActionResult> GetByMonth([FromBody] int? month, int? year)
+        {
+            var result = await _service.OrderStatisticsByMonth(month, year);
+            return Ok(result);
+        }
+        [HttpGet("by-year")]
+        public async Task<IActionResult> GetByYear([FromBody] int? year)
+        {
+            var result = await _service.OrderStatisticsByYear(year);
+            return Ok(result);
+        }
+        [HttpGet("best-seller")]
+        public async Task<IActionResult> GetBestSellers([FromBody] int number)
+        {
+            var result = await _service.GetBestSeller(number);
+            return Ok(result);
+        }
+    }
+}
---
+++ MilkTeaPosManagement.Api/Services/Interfaces/IStatisticService.cs
@@ -0,0 +1,14 @@
+﻿using MilkTeaPosManagement.Api.Helper;
+using MilkTeaPosManagement.Domain.Models;
+
+namespace MilkTeaPosManagement.Api.Services.Interfaces
+{
+    public interface IStatisticService
+    {
+        Task<(List<object>, List<object>, int, decimal?)> OrderStatisticsByDate(DateTime? date);
+        Task<(List<object>, List<object>, int, decimal?)> OrderStatisticsByWeek(DateTime? fromDate, DateTime? toDate);
+        Task<(List<object>, List<object>, int, decimal?)> OrderStatisticsByMonth(int? month, int? year);
+        Task<(List<object>, List<object>, int, decimal?)> OrderStatisticsByYear(int? year);
+        Task<List<object>> GetBestSeller(int number);
+    }
+}
