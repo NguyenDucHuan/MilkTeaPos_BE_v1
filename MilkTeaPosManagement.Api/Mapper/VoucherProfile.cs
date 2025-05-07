a66f103a340e6f8bbaba3e98dd6d9d873f3ac5bb
NguyenDucHuan <NguyenDucHuan14012003@gmail.com>
2025-05-07 14:22:35 +0700

    voucher fix for response mapper and DB

---
+++ MilkTeaPosManagement.Api/Mapper/VoucherProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MilkTeaPosManagement.Api.Models.VoucherMethod;
+using MilkTeaPosManagement.Domain.Models;
+
+namespace MilkTeaPosManagement.Api.Mapper
+{
+    public class VoucherProfile : Profile
+    {
+        public VoucherProfile()
+        {
+            CreateMap<Voucher, VoucherResponse>();
+        }
+    }
+}
